8b2d4e6a9534dcb77e831c42644eeb35d9803b87
ConnorWhite <connor.bcw@gmail.com>
2017-02-11 15:40:37 -0600

    Restructured & styled 404 page

--- 404.php
+++ 404.php
@@ -1,11 +1,14 @@
 <?php get_header(); ?>
 <div id="main">
 	<div class="four-o-four">
-		<h2>404</h2>
-		<h3>Page not found :(</h3>
-		<div class="button">
-			<a href="/">Home</a>
+		<div class="wrap">
+			<h2>404</h2>
+			<h3>Page not found :(</h3>
+			<div class="button-div">
+				<a class="button" href="/">Home</a>
+			</div>
 		</div>
 	</div>
+	<?php include "contact.php" ?>
 </div>
 <?php get_footer(); ?>
