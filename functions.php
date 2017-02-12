5ad727a1d415531dbede6e0501b5f77a97d36964
ConnorWhite <connor.bcw@gmail.com>
2017-02-11 21:46:22 -0600

    WP Login (full custom)

--- functions.php
+++ functions.php
@@ -14,9 +14,13 @@ if (function_exists('add_theme_support')) {
 /* Custom Login Screen-------- */
 function my_login_stylesheet() {
     wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
-		wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/scripts/style-login.js' );
 }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+
+function my_login_logo_url() {
+	return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
 
 /* List Categories------------*/
 function add_post_cats($postcats){
--- scripts/style-login.js
+++
@@ -1,4 +0,0 @@
-var login = document.getElementById("login");
-var h1 = login.getElementsByTagName("h1")[0];
-var a = h1.getElementsByTagName("a")[0];
-a.href="/";
