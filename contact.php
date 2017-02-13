28b688b7620ca6bc2a412ae99bb88ec4f3d245a8
ConnorWhite <connor.bcw@gmail.com>
2017-02-12 18:51:48 -0600

    Contact Form

--- contact.php
+++ contact.php
@@ -2,6 +2,47 @@
 	<div class="wrap">
 		<h2>Let's Work Together!</h2>
 		<p>I am currently accepting projects and would love to discuss yours.</p>
-		<a class="button" href="mailto:connor@connorwhite.org">Email</a>
+		<p id="thanks">Thanks! Your message has been recorded.</p>
+		<form id="contact-form" action="" method="POST" onsubmit="return false;">
+			<div class="info">
+				<div class="field">
+					<p>Name:</p>
+					<input id="form-name" type="text" name="name" />
+				</div>
+				<div class="field">
+					<p>Email:</p>
+					<input id="form-email" type="text" name="email" />
+				</div>
+			</div>
+			<textarea id="form-message" name="message" placeholder="Message:" rows="5"></textarea>
+			<div class="send">
+				<input id="form-submit" type="submit" name="submit" value="Send" onclick="sendEmail()" />
+			</div>
+		</form>
 	</div>
 </div>
+<script>
+	function sendEmail(){
+		var xhttp = new XMLHttpRequest();
+		var url = "<?php bloginfo('template_url'); ?>" + "/mail.php";
+		var params = "name=" + document.getElementById('form-name').value + "&email=" + document.getElementById('form-email').value + "&message=" + document.getElementById('form-message').value;
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200){
+				document.getElementById('contact-form').style.opacity = 0;
+				var name = document.getElementById('form-name').value;
+				if(/^[A-Za-z\s]+$/.test(name)){
+					name = name.split(" ")[0];
+					document.getElementById('thanks').innerHTML = "Thanks " + name + "! Your message has been sent.";
+				}
+				setTimeout(function(){
+					document.getElementById('contact-form').style.visibility = "hidden";
+					document.getElementById('form-message').style.height = "0";
+					document.getElementById('contact-form').style.height = "0";
+					document.getElementById('thanks').style.display = "block";
+				}, 1000);
+			}
+		};
+		xhttp.open("POST", url, true);
+		xhttp.send(params);
+	}
+</script>
---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+  $name = $email = $message = "";
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+  	$name = validate($_POST["name"]);
+  	$email = validate($_POST["email"]);
+  	$message = validate($_POST["message"]);
+  	mail("connor.bcw@gmail.com", "New Message from " . $name . " at " . $email, $message);
+  }
+  function validate($data){
+  	$data = trim($data);
+  	$data = stripslashes($data);
+  	$data = htmlspecialchars($data);
+  	return $data;
+  }
+?>
