25b4cb39deea1aeadbf7f4b5a345bf3069dbc509
ConnorWhite <connor.bcw@gmail.com>
2017-02-14 19:24:47 -0600

    Revised meta tags

--- header.php
+++ header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html id="html">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <?php if(is_front_page()){ ?>
         	<title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
         <?php } else { ?>
         	<title><?php wp_title(); ?> | <?php bloginfo('name'); ?></title>
         <?php } ?>
         <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon.ico" />
-        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0">
-        <meta name="Revisit-After" content="7 Days">
+        <meta description="A showcase of the work of Connor White, a web developer specializing in Wordpress." />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="robots" content="index,follow">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0">
         <?php wp_head(); ?>
     </head>
     <body>
